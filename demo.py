e74ea863e5b2a8a55cc80231ebde770ba0cb8bc6
arijit <ar999557@gmail.com>
2024-03-07 20:25:12 +0530

    commit3

--- demo.py
+++ demo.py
@@ -1,1 +1,8 @@
-print("arijit dey")
+import os
+path=("notebook/research.ipynb")
+dir,file=os.path.split(path)
+print(dir)
+print(file)
+os.makedirs(dir,exist_ok=True)  
+with open(file,'w') as f:
+    pass
