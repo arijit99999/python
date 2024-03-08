314d5a68e7c60843048f40c2f944608342469097
arijit <ar999557@gmail.com>
2024-03-08 12:56:32 +0530

    commit13

---
+++ src/dimondpriceprediction/components/data_transformation.py
@@ -0,0 +1,5 @@
+class datatransformation:
+    def __init__(self):
+        pass
+    def get_datatransformation(self):
+        pass  
---
+++ src/dimondpriceprediction/components/model_trainer.py
@@ -0,0 +1,5 @@
+class modeltraining:
+    def __init__(self):
+        pass
+    def initiate_modeltraining(self):
+        pass  
