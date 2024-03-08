2f6f9451e7cacb809bd64245160e592c9bcf1f43
arijit <ar999557@gmail.com>
2024-03-08 17:50:21 +0530

    cimmit15

--- src/dimondpriceprediction/logger.py
+++ src/dimondpriceprediction/logger.py
@@ -1,3 +1,12 @@
 import os
 import logging
 from datetime import datetime
+log_file=f"{datetime.now().strftime('%d_%m_%y_%H_%M_%S')}.log"
+log_path=os.path.join(os.getcwd(),'logs')
+os.makedirs(log_path,exist_ok=True)
+log_file_path=os.path.join(log_path,log_file)
+
+
+
+logging.basicConfig(level=logging.INFO,filename=log_file_path,
+format=('%(lineno)d--%(levelname)s--%(asctime)s--%(message)s'))
