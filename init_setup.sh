987da3731a2f6b3da01d91559d19f1cdde2febf9
arijit <ar999557@gmail.com>
2024-03-08 09:01:36 +0530

    commit8

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,10 @@
 echo[$(date)]:'START'
 
 echo[$(date)]:'creating env with python 3.11 version'
-conda create --p ./env python=3.11 -y
+conda create --prefix ./env python=3.11 -y
 
 echo[$(date)]:'activating the environment'
-conda activate ./env
+sorce activate ./env
 
 echo[$(date)]:'installing the env requrements'
 pip install -r requirements.txt
