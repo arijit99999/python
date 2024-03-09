6f77582479947b6c62df8924eb9e57c262acc81a
arijit <ar999557@gmail.com>
2024-03-09 19:17:32 +0530

    commit21

--- src/dimondpriceprediction/components/data_transformation.py
+++ src/dimondpriceprediction/components/data_transformation.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 from src.dimondpriceprediction.logger import logging
 from src.dimondpriceprediction.exception import customexception
 from dataclasses import dataclass
@@ -8,6 +9,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+from src.dimondpriceprediction.utlis.utils import save_object
 
 
 @dataclass
@@ -20,7 +22,40 @@ class datatransformation:
 
     def get_datatransformation(self):
         try:
-            pass
+            logging.info('data transformation initiated')
+            categrical_cols=['cut', 'color', 'clarity']
+            numerical_cols=['carat', 'depth', 'table', 'x', 'y', 'z', 'price']
+
+            cut_category= ['Premium' 'Very Good' 'Ideal' 'Good' 'Fair']
+            color_category= ['F' 'J' 'G' 'E' 'D' 'H' 'I']
+            clarity_category= ['VS2' 'SI2' 'VS1' 'SI1' 'IF' 'VVS2' 'VVS1' 'I1']
+
+            logging.info('pipeline  has been initiated')
+
+            num_pipeline=Pipeline(
+                steps=[
+                    ('impute',SimpleImputer(strategy='median')),
+                    ('scaling',StandardScaler())
+                ]
+            )
+            cat_pipeline=Pipeline(
+                steps=[
+                    ('impute',SimpleImputer(strategy='most_frequent')),
+                    ('odinal_encoder',OrdinalEncoder(categories=[cut_category,color_category,clarity_category])),
+                    ('scaling',StandardScaler())
+                ]
+            )
+
+            preprocessor=ColumnTransformer(
+                [
+                    ('num_pipeline',num_pipeline,numerical_cols),
+                    ('cat_pipeline',cat_pipeline,categrical_cols)
+                ]
+            )
+
+            return preprocessor
+
+
         except Exception as e:
            logging.info("exception has been triggered at data transformation stage")
            raise customexception(e,sys)
@@ -34,8 +69,34 @@ class datatransformation:
             logging.info("reading of test and traning data is complited")
             logging.info(f'train dataframe head : \n{train_d.head().to_string()}')
             logging.info(f'test dataframe head : \n{test_d.head().to_string()}')
+            
+            preprocessor_obj=self.get_datatransformation()
 
+            traget_col_name="price"
+            drop_col=[traget_col_name,"id"]
 
+            input_feature_for_train_data=train_d.drop(columns=drop_col,axis=1)
+            target_feature_for_train_data=train_d[traget_col_name]
+            input_feature_for_test_data=test_d.drop(columns=drop_col,axis=1)
+            target_feature_for_test_data=train_d[traget_col_name]
+
+            input_feature_for_train_data_preprocessor=preprocessor_obj.fit_transform(input_feature_for_train_data)
+            input_feature_for_test_data_preprocessor=preprocessor_obj.transform(input_feature_for_test_data)
+
+            logging.info('applied preprocessing object on train and test data')
+
+            train_arr=np.c_[input_feature_for_train_data_preprocessor,np.array(target_feature_for_train_data)]
+            test_arr=np.c_[input_feature_for_test_data_preprocessor,np.array(target_feature_for_test_data)]
+            
+            save_object(file_path=self.data_transformation_config.preprocessor_path,
+                        obj=preprocessor_obj)
+
+            logging.info('preprocessor file saved as pickle file')
+
+            return(
+                train_arr,
+                test_arr
+            )
         except Exception as e:
            logging.info("exception has been triggered at data transformation stage")
            raise customexception(e,sys)
--- src/dimondpriceprediction/components/model_trainer.py
+++ src/dimondpriceprediction/components/model_trainer.py
@@ -1,5 +1,64 @@
+import os
+import sys
+import pandas as pd
+from src.dimondpriceprediction.logger import logging
+from src.dimondpriceprediction.exception import customexception
+from dataclasses import dataclass
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+from src.dimondpriceprediction.utlis.utils import save_object
+from src.dimondpriceprediction.utlis.utils import evaluate_model
+
+from sklearn.linear_model import LinearRegression,Lasso,Ridge,ElasticNet
+
+
+@dataclass
+class model_training_config:
+    train_model_path=os.path.join('artifacts','model.pkl')
+
 class modeltraining:
     def __init__(self):
-        pass
+        self.model_train_config=model_training_config()
     def initiate_modeltraining(self):
-        pass  
+        try:
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            
+            models={
+            'LinearRegression':LinearRegression(),
+            'Lasso':Lasso(),
+            'Ridge':Ridge(),
+            'Elasticnet':ElasticNet()
+        }
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
+            print(model_report)
+            print('\n====================================================================================\n')
+            logging.info(f'Model Report : {model_report}')
+
+            # To get best model score from dictionary 
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            
+            best_model = models[best_model_name]
+
+            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print('\n====================================================================================\n')
+            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+
+            save_object(
+                 file_path=self.model_train_config.train_model_path,
+                 obj=best_model
+            )
+
+
+        except Exception as e:
+            logging.info('Exception occured at Model Training')
+            raise customexception(e,sys)
